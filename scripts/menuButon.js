d4bdafa1ebf8ee15545fb5f1c4274810633c3d4e
Matrer <koziol.marcin96@gmail.com>
2019-03-24 18:48:48 +0100

    hide

--- scripts/menuButon.js
+++ scripts/menuButon.js
@@ -5,3 +5,13 @@ $(document).ready(function(){
       $('#listContenter').fadeToggle("fast");
   });
 });
+
+$(document).mouseup(function(e)
+{
+    var container = $('#list');
+    var listContenter = $('#listContenter');
+    if (!container.is(e.target) && container.has(e.target).length === 0)
+    {
+        listContenter.hide();
+    }
+});
